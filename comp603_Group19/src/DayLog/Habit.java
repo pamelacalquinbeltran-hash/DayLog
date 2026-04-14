42f63eb7db8f0bcc935b6a9281a2609f1d21523f
TaranehTsh <taraneh.drive@gmail.com>
2026-04-14 13:03:24 +1200

    Changes to the habit class

--- comp603_Group19/src/DayLog/Habit.java
+++ comp603_Group19/src/DayLog/Habit.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DayLog;
 
-/**
- *
- * @author taran
- */
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Habit {
-    
+
+    private String name;
+    private String frequency; // e.g. Daily, Mon/Wed/Fri
+    private Set<LocalDate> completedDates;
+
+    public Habit(String name, String frequency) {
+        this.name = name;
+        this.frequency = frequency;
+        this.completedDates = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public boolean isCompletedOn(LocalDate date) {
+        return completedDates.contains(date);
+    }
+
+    public void toggleCompletion(LocalDate date) {
+        if (completedDates.contains(date)) {
+            completedDates.remove(date);
+        } else {
+            completedDates.add(date);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " [" + frequency + "]";
+    }
 }
